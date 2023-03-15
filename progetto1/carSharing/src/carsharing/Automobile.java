c38f3da06ba659d59429b81156af0db3cab67a37
teoconte <124182490+teoconte@users.noreply.github.com>
2023-03-15 11:47:47 +0100

    Add files via upload

--- progetto1/carSharing/src/carsharing/Automobile.java
+++ progetto1/carSharing/src/carsharing/Automobile.java
@@ -1,7 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package carsharing;
+
+/**
+ *
+ * @author matte
+ */
 public class Automobile {
+
     private String targa;
-    private int nViaggi=0;
+    private int nViaggi = 0;
     private String pos;
 
     public Automobile(String targa, String pos) {
@@ -12,24 +22,30 @@ public class Automobile {
     public String getTarga() {
         return targa;
     }
+
     public int getnViaggi() {
         return nViaggi;
     }
+
     public String getPos() {
         return pos;
     }
+
     public void setnViaggi(int nViaggi) {
         this.nViaggi = nViaggi;
     }
+
     public void setPosPark(String pos) {
         this.pos = pos;
     }
+
     @Override
     public String toString() {
         return "Automobile{" + "targa=" + targa + ", nViaggi=" + nViaggi + ", pos=" + pos + '}';
     }
-    
-    public void viaggia(){
+
+    public void viaggia() {
         nViaggi++;
     }
+
 }
--- progetto1/carSharing/src/carsharing/Parcheggio.java
+++ progetto1/carSharing/src/carsharing/Parcheggio.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package carsharing;
+
 import java.util.ArrayList;
 
 /**
@@ -10,21 +11,22 @@ import java.util.ArrayList;
  * @author matte
  */
 public class Parcheggio {
+
     private int nPosti;//posti massimi parcheggio
     private String nome;//nome del parcheggio
     private ArrayList<Automobile> automobili;//array delle automobili presenti nel parcheggio
-    
-    public Parcheggio(String nome, int nPosti){
-        this.nome=nome;
-        this.nPosti=nPosti;
-        this.automobili=new ArrayList<Automobile>();
+    private static int nParcheggio=0;
+
+    public Parcheggio(String nome, int nPosti) {
+        this.nome = nome;
+        this.nPosti = nPosti;
+        this.automobili = new ArrayList<Automobile>();
     }
-    
-    public boolean AddAuto(Automobile auto){
-        if(automobili.size() == nPosti){
+
+    public boolean AddAuto(Automobile auto) {
+        if (automobili.size() == nPosti) {
             return false;
-        }
-        else{ 
+        } else {
             automobili.add(auto);
             return true;
         }
@@ -40,7 +42,12 @@ public class Parcheggio {
 
     @Override
     public String toString() {
-        return "Parcheggio{" + "nome=" + nome + ", nPosti=" + nPosti + ", automobili=" + automobili + '}';
+        String stringa = "{" + "nome=" + nome + ", nPosti=" + nPosti + ", automobili= ";
+        for(int i=0; i<automobili.size();i++){
+            stringa+=automobili.get(i).toString();
+        }
+        stringa+="},";
+        return stringa;
     }
 
     public ArrayList<Automobile> getAutomobili() {
@@ -50,12 +57,57 @@ public class Parcheggio {
     public void setAutomobili(ArrayList<Automobile> automobili) {
         this.automobili = automobili;
     }
-    
-    public int getCount(){
+
+    public int getCount() {
         return automobili.size();
     }
+    public boolean elimina(Automobile a){
+       return automobili.remove(a);
+    } 
     
+    public boolean elimina(ArrayList<Automobile> array){
+        boolean sem=false;
+        if(!(array.isEmpty())){
+            sem = true;
+            for(Automobile a:array){
+                automobili.remove(a);
+            }
+        }
+        else sem=false;
+        return sem;
+    } 
     
+    public boolean elimina(int indice){
+        boolean sem=false;
+        if(indice == -1)
+            sem=false;
+        else{ automobili.remove(indice); sem=true;}
+        
+        return sem;
+    }
+            
+            
+    public Automobile trova(String targa){
+        for(Automobile a:automobili){
+            if(a.getTarga().equals(targa))
+                return a;
+        }
+        return null;
+    }
     
+    public int trova(Automobile a){
+        int indice=automobili.indexOf(a);
+        return indice;
+    }
     
+    public ArrayList<Automobile> trova(int n){
+        ArrayList<Automobile> array = new ArrayList<Automobile>();
+        for(Automobile a:automobili){
+            if(a.getnViaggi()>=n)
+                array.add(a);
+        }
+        return array;
+    }
+    
+
 }
